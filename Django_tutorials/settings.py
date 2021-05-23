dd93120dea423f7eb881bd2d8f1df7f0d8cbc094
Deep201620 <76123471+Deep201620@users.noreply.github.com>
2021-05-23 11:38:44 +0530

    Update settings.py

--- Django_tutorials/settings.py
+++ Django_tutorials/settings.py
@@ -108,7 +108,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'infotechupdatemedia@gmail.com'
-EMAIL_HOST_PASSWORD = 'toaster16'
+EMAIL_HOST_PASSWORD = '*********'
 
 
 # Internationalization
